50f3c16f7b7d258c6f8a0b10c79ca6bb2523ff1c
abderraouf2 <79739572+abderraouf2@users.noreply.github.com>
2021-08-11 17:31:41 +0100

    fixed

--- src/Components/CardList.js
+++ src/Components/CardList.js
@@ -1,9 +1,9 @@
 import React from 'react';
 import Card from './Card';
 const CardList=({robots})=>{
-  if(true){
-    throw new Error("noooooooo");
-  }
+  // if(true){
+  //   throw new Error("noooooooo");
+  // }
   const CardComponent= robots.map((user,i)=>{
     return <Card key={i} id={robots[i].id} name={robots[i].name} 
     email={robots[i].email}/>
